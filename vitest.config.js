f00a7a5501eb960a3f0aa709e3ada372dc8ceebd
zaynom <ouaziz0626@gmail.com>
2025-01-01 19:53:47 +0100

    chore: resoleve `src` alias for test files

--- vitest.config.js
+++ vitest.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    // globals: true,
+  resolve: {
+    alias: {
+      "@src": "/src",
+    },
   },
 });
