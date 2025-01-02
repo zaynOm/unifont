adaf74390242e25bb7190e164d9a7b2f175f7464
zaynom <ouaziz0626@gmail.com>
2025-01-02 18:34:37 +0100

    feat: update underline formatting to make it look better
    - convert text to monospace first to make the underline connected

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,8 @@ type Font = Record<Styles, UnicodeCharMap | number>;
 const fonts: Font = {
   bold: { upper: 119743, lower: 119737, number: 120734 },
   italic: { upper: 119795, lower: 119789, number: null },
-  underline: { upper: 119743, lower: 119737, number: null },
   monospace: { upper: 120367, lower: 120361, number: 120774 },
+  underline: 818,
 };
 
 const missingChars: Partial<Record<Styles, Record<string, string>>> = {
@@ -32,6 +32,19 @@ export function format(style: Styles, text: string) {
     } else if (fonts[style].number && /[0-9]/.test(c))
       res.push(code + fonts[style].number);
     else res.push(code);
+  }
+
+  return String.fromCodePoint(...res);
+}
+
+function underline(text: string) {
+  // convert the text to monospace first to get a nice connected underline
+  const monotext = format("monospace", text);
+  let res = [];
+  for (const char of monotext) {
+    const codePoints = [...char];
+    res.push(...codePoints.map((cp) => cp.codePointAt(0)!));
+    res.push(+fonts.underline);
   }
 
   return String.fromCodePoint(...res);
